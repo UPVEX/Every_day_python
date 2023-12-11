36577e981ea85cf4009968e8e8d6f68b0f52e80b
unknown <nimamollayi@gmail.com>
2023-12-11 23:12:07 +0330

    21th day

---
+++ loknat_zaban.py
@@ -0,0 +1,17 @@
+# این برنامه از کاربر یه کلمه گرفته و ان را به صورت فردی که لکنت زبان دارد چاپ میکند
+
+
+
+def stutter(word):
+    if len(word) >= 2:
+        stuttered_word = word[:2] + '... ' + word[:2] + '... ' + word + '?'
+        return stuttered_word
+    else:
+        return "Word must have at least two letters."
+
+# دریافت ورودی از کاربر
+user_input = input("Enter a word: ")
+
+# فراخوانی تابع و نمایش خروجی
+result = stutter(user_input)
+print(result)
