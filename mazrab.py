05119405e39684dd181783023a7aa449a057e6d7
unknown <nimamollayi@gmail.com>
2024-02-07 22:52:27 +0330

    51th day

---
+++ mazrab.py
@@ -0,0 +1,13 @@
+# برنامه زیر دو عدد میگرد و برسی میکند که ایا مضرب یکدیگر  هستند یا خیر
+
+
+
+
+x = int(input("enter a number ==> "))
+y = int(input("enter a number ==> "))
+
+if (y % x == 0):
+    print("yes")
+else:
+    print("no")
+
