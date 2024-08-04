9b7add419c2030784844ed7d7158211bbf23526c
unknown <nimamollayi@gmail.com>
2024-08-04 17:38:59 +0330

    78th day

---
+++ sudoku.py
@@ -0,0 +1,54 @@
+# این برنامه یک سودوکو 9 در 9 را حل میکند
+
+
+
+
+def is_valid(board, row, col, num):
+    # بررسی سطر
+    for x in range(9):
+        if board[row][x] == num:
+            return False
+
+    # بررسی ستون
+    for x in range(9):
+        if board[x][col] == num:
+            return False
+
+    # بررسی بلوک 3x3
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+def sudoku(puzzle):
+    solve_sudoku(puzzle)
+    return puzzle
+
+# تست
+puzzle = [[5,3,0,0,7,0,0,0,0],
+          [6,0,0,1,9,5,0,0,0],
+          [0,9,8,0,0,0,0,6,0],
+          [8,0,0,0,6,0,0,0,3],
+          [4,0,0,8,0,3,0,0,1],
+          [7,0,0,0,2,0,0,0,6],
+          [0,6,0,0,0,0,2,8,0],
+          [0,0,0,4,1,9,0,0,5],
+          [0,0,0,0,8,0,0,7,9]]
+
+print(sudoku(puzzle))
